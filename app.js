eb6cb69b7a5ae10b186e63a58f6ab14316b6952d
ranveerjaat989 <ranveerjaat989@gmail.com>
2024-01-30 17:53:56 +0530

    Change app.js file

--- app.js
+++ app.js
@@ -1,14 +1,61 @@
-setTimeout(()=>{
-    let dino=document.querySelector('.game-dino');
-    let current=true;
-    let score=0;
-    let song= new Audio('./icon/gameBackground.mp3');
-    let outSong=new Audio('./icon/gameover.mp3');
-    
-    
+
+let endgame=0;
+let dino1=document.querySelector('.game-dino');
+let dragon=document.querySelector('.game-dragon');
+let gameOver=document.querySelector('.game-title h2');
+let dx,dy,drx,dry,offsetX, offsetY,gameScore,anidur, dino,current,dinoX,score
+// let dy
+// let drx
+// let dry
+// let offsetX
+// let offsetY
+let dragon1=document.querySelector('.game-dragon');
+// let gameScore
+// let anidur
+let song= new Audio('./icon/gameBackground.mp3');
+let outSong=new Audio('./icon/gameover.mp3');
+// let dino
+// let current
+// let score
+// let dinoX
+
+const start=document.querySelector('.StartButton');
+
+dragon1.classList.remove('run-dragon');
+dragon1.style.left="800px";
+start.addEventListener('click',()=>{
+    if(start.innerText!="Restart"){
+        start.style.display="none";
+        dragon1.classList.add('run-dragon');
+        gameCode();
+    }
+    else{
+       gameOver.style.visibility='hidden';
+        dragon1.classList.add('run-dragon');
+        dragon.style.animationDuration="6s"
+        score=0;
+        gameScore.innerText='Game Svore: '+score; 
+        dinoleft1();
+        gameCode();
+        start.style.display="none";
+        endgame=0;
+    }
+})
+function dinoleft1(){
+    dino.style.bottom='80px';
+}
+
+const gameCode=()=>{
+     dino=document.querySelector('.game-dino');
+    current=true;
+   score=0;
+ 
     document.onkeydown= (e)=>{
-        outSong.pause();
-        song.play();
+        if(endgame!=1){
+            outSong.pause();
+            song.play();
+        }
+       
         if(e.keyCode==38)
         {
             console.log(e.keyCode);
@@ -20,41 +67,44 @@ setTimeout(()=>{
             },600)
           
         }
-        else if(e.keyCode==39)
+        else if(e.keyCode==39&&endgame!=1)
         {
            
-            let dinoX=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
+            dinoX=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
             dino.style.left=dinoX+180+'px';
         }
-        else if(e.keyCode==37)
+        else if(e.keyCode==37&&endgame!=1)
         {
-            let dinoX=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
+          dinoX=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
             dino.style.left=dinoX-180+'px';
         }
     }
     // song.autoplay();
     
     setInterval(()=>{
-        let dino1=document.querySelector('.game-dino');
-        let dragon=document.querySelector('.game-dragon');
-        let gameOver=document.querySelector('.game-title h2');
-        let dx=parseInt(window.getComputedStyle(dino1,null).getPropertyValue('left'));
-        let dy=parseInt(window.getComputedStyle(dino1,null).getPropertyValue('top'));
+       //  dino1=document.querySelector('.game-dino');
+      //  dragon=document.querySelector('.game-dragon');
+        // gameOver=document.querySelector('.game-title h2');
+        dx=parseInt(window.getComputedStyle(dino1,null).getPropertyValue('left'));
+         dy=parseInt(window.getComputedStyle(dino1,null).getPropertyValue('top'));
      
-        let drx=parseInt(window.getComputedStyle(dragon,null).getPropertyValue('left'));
-        let dry=parseInt(window.getComputedStyle(dragon,null).getPropertyValue('top'));
-         let offsetX=Math.abs(drx-dx);
-         let offsetY=Math.abs(dry-dy);
+       drx=parseInt(window.getComputedStyle(dragon,null).getPropertyValue('left'));
+         dry=parseInt(window.getComputedStyle(dragon,null).getPropertyValue('top'));
+         offsetX=Math.abs(drx-dx);
+          offsetY=Math.abs(dry-dy);
          if(offsetX<=120 && offsetY<=75){
             gameOver.style.visibility='visible';
-            let dragon1=document.querySelector('.game-dragon');
+            // dragon1=document.querySelector('.game-dragon');
             dragon1.classList.remove('run-dragon');
              dragon1.style.left=offsetX+100+'px';
              dinoleft0();
-             
+             dragon1.style.left="800px";
+            //  dino1.style.display="none";
+              endgame=1;
               song.pause();
               outSong.play();
-              
+              start.style.display="inline";
+              start.innerText="Restart";
               setTimeout(()=>{
                 outSong.pause();
               },6000);
@@ -65,10 +115,9 @@ setTimeout(()=>{
             current=false;
             setTimeout(()=>{
                 current=true;
-    
             },500)
             setTimeout(()=>{
-                let anidur=parseFloat(window.getComputedStyle(dragon,null).getPropertyValue('animation-duration'));
+                 anidur=parseFloat(window.getComputedStyle(dragon,null).getPropertyValue('animation-duration'));
                 if(anidur>=3.7){
                     console.log(anidur);
                     newdur=anidur-0.1;
@@ -78,18 +127,17 @@ setTimeout(()=>{
                     newdur=anidur+2;
                     dragon.style.animationDuration=newdur+'s';
                 }
-              
             },400)
-           
          }
-         
-    },30)  
+    },10)  
     function Score(){
         score++;
-        let gameScore=document.querySelector('.game-score');
+        gameScore=document.querySelector('.game-score');
         gameScore.innerText='Game Svore: '+score; 
     }
     function dinoleft0(){
         dino.style.bottom='-80px';
     }
-},1000);
+   
+}
+   
